a6462de4eadab72d60b0fe939d8921bf5989846e
marriet-aho <marrietaho2020@gmail.com>
2024-12-13 13:52:55 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,97 @@
+
+// function 
+// let name=prompt("what is your name?");
+// alert("Welcome" ${name} "to Rieta's Bank");
+
+function show(){
+    let name=prompt("what is your name?");
+   alert(`Welcome ${name} to Rieta's Bank`);
+}
+let who =document.getElementById("why");
+who.addEventListener("click",show);
+
+
+function hidetext(){
+    let aho = document.getElementById("hide");
+    aho.style.display = "block";
+
+    let gloria = document.getElementById("Read");
+    gloria.style.display = "block";
+    let andrea = document.getElementById("but1");
+    andrea.style.display = "none";
+
+}
+let marriet = document.getElementById("but1");
+    marriet.addEventListener("click", hidetext);
+
+
+ function lup(){
+    let gloria = document.getElementById("hide");
+    gloria.style.display = "none";
+    let ayaana = document.getElementById("Read");
+    ayaana.style.display = "none";
+    let abena = document.getElementById("but1");
+    abena.style.display = "block";
+    
+ }   
+    let rabi = document.getElementById("Read");
+    rabi.addEventListener("click", lup);
+
+
+    // 
+    function Fair(){
+        let enyo = document.getElementById("hid");
+        enyo.style.display = "block";
+    
+        let daniela = document.getElementById("fruit");
+        daniela.style.display = "block";
+        let saraphine = document.getElementById("but2");
+        saraphine.style.display = "none";
+        let abena = document.getElementById("but2");
+        abena.style.display = "none";
+    
+    }
+    let baaba = document.getElementById("but2");
+        baaba.addEventListener("click", Fair);
+    
+    
+     function equip(){
+        let daniela = document.getElementById("hid");
+        daniela.style.display = "none";
+        let sophia = document.getElementById("fruit");
+        sophia.style.display = "none";
+        let koomson = document.getElementById("but2");
+        koomson.style.display = "block";
+        
+     }   
+        let rachel = document.getElementById("fruit");
+        rachel.addEventListener("click", equip);
+
+
+        // 
+        function school(){
+            let debbie = document.getElementById("ide");
+            debbie.style.display = "block";
+        
+            let selina = document.getElementById("food");
+            selina.style.display = "block";
+            let portia = document.getElementById("but3");
+            portia.style.display = "none";
+        
+        }
+        let stella = document.getElementById("but3");
+            stella.addEventListener("click", school);
+        
+        
+         function gimpa(){
+            let patience = document.getElementById("ide");
+            patience.style.display = "none";
+            let lawrencia = document.getElementById("food");
+            lawrencia.style.display = "none";
+            let alfredina = document.getElementById("but3");
+            alfredina.style.display = "block";
+            
+         }   
+            let comfort = document.getElementById("food");
+            comfort.addEventListener("click", gimpa);
+        
